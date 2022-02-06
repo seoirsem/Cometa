7818ab6f963fb2c14c54e7fdf7811aac62ff6cb8
seoirsem <54598583+seoirsem@users.noreply.github.com>
2022-02-06 14:53:03 +0000

    Asteroid Line Renderer

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -7,10 +7,13 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    GameObject asteroidOutlines;
     // Start is called before the first frame update
     Mesh mesh;
+    Mesh colliderMesh;
     public Vector3[] meshVertices;
     public int[] meshTriangles;
+    public int[] meshIndices;
     float mass = 1;
     Vector3 rotation;
     public float rotationRate;
@@ -19,11 +22,14 @@ public class Asteroid : MonoBehaviour
     Vector2 worldSize;
     Vector2 location;
     List<GameObject> asteroidPack;
+    MeshCollider meshCollider;
 
 
 
     public void OnSpawn(int size, Vector2 location, List<GameObject> asteroidPack, GameObject mainAsteroid, Vector3 velocity)
     {
+        asteroidOutlines = this.gameObject.transform.Find("AsteroidOutline").gameObject;
+
         this.asteroidPack = asteroidPack;
         asteroidgo = this.gameObject;
         this.velocity = velocity;
@@ -34,7 +40,7 @@ public class Asteroid : MonoBehaviour
         if (this.gameObject == mainAsteroid)
         {
             //in Unity trianges are drawn clockwise
-            this.rotationRate = Mathf.Pow(Random.Range(0f, 1f),2.2f) * 400;//random rotation rate
+            this.rotationRate = Mathf.Pow(Random.Range(-1f, 1f),2f) * 250;//random rotation rate
             DrawAsteroid(size);
         }
         else
@@ -49,37 +55,54 @@ public class Asteroid : MonoBehaviour
     void DrawAsteroid(int size)
     {
         //random int including the starting number, excluding the finishing number
-        int numberOfSides = Random.Range(4, 11);
+        int numberOfSides = Random.Range(5, 13);
 
         float radius = size / 6f;
 
-        meshVertices = new Vector3[numberOfSides];
+        meshVertices = new Vector3[numberOfSides+1];
         float angle = 0;
+        Vector3 randomOffset;
+        float randomAngleOffset;
+        float randomRadius;
         for (int i = 0; i < numberOfSides; i++)
         {//drawing a simple circle
             angle = 2 * Mathf.PI * i / numberOfSides;
-            meshVertices[i] = new Vector3(radius*Mathf.Sin(angle), radius * Mathf.Cos(angle), 0);
+            randomRadius = Random.Range(0.6f*radius,1.2f*radius);//allowable range of radii
+            randomAngleOffset = Random.Range(angle + 2 * Mathf.PI * 0.5f / numberOfSides, angle - 2 * Mathf.PI * 0.5f / numberOfSides);
+            randomOffset = new Vector3(randomRadius*Mathf.Sin(randomAngleOffset), randomRadius * Mathf.Cos(randomAngleOffset), 0);
+            meshVertices[i] = randomOffset;
+            //new Vector3(radius*Mathf.Sin(angle), radius * Mathf.Cos(angle), 0);
         
         }
-        
+        meshVertices[numberOfSides] = new Vector3(0, 0, 0); // the centre of the object is the last vertex in the list
+
+
         meshTriangles = new int[3*numberOfSides];
-        for (int i = 0; i < numberOfSides - 2; i++)
+        for (int i = 0; i < numberOfSides - 1; i++)
         {
             meshTriangles[3*i] = i;
             meshTriangles[3*i+1] = i+1;
-            meshTriangles[3*i+2] = i+2;
+            meshTriangles[3*i+2] = numberOfSides;
         }
-        meshTriangles[3 * (numberOfSides - 2)] = numberOfSides - 2;
-        meshTriangles[3 * (numberOfSides - 2) + 1] = numberOfSides - 1;
-        meshTriangles[3 * (numberOfSides - 2) + 2] = 0;
         meshTriangles[3 * (numberOfSides - 1)] = numberOfSides - 1;
         meshTriangles[3 * (numberOfSides - 1) + 1] = 0;
-        meshTriangles[3 * (numberOfSides - 1) + 2] = 1;
+        meshTriangles[3 * (numberOfSides - 1) + 2] = numberOfSides;
+
+        meshIndices = new int[2 * numberOfSides];
+        for (int i = 0; i < numberOfSides - 1; i++)
+        {
+            meshIndices[2 * i] = i;
+            meshIndices[2 * i + 1] = i + 1;
+        }
+        meshIndices[2 * (numberOfSides - 1) - 1] = numberOfSides - 1;
+        meshIndices[2 * (numberOfSides - 1)] = 0;
+
 
 
 
         //Notes on drawing lines
         //https://docs.unity3d.com/ScriptReference/MeshTopology.html
+        // code to do so: https://www.h3xed.com/programming/create-2d-mesh-outline-in-unity-silhouette
 
         //notation for drawing a square
         //meshVertices = new Vector3[]
@@ -97,25 +120,45 @@ public class Asteroid : MonoBehaviour
         //};
 
 
-        DrawMesh(meshVertices, meshTriangles);
+        DrawMesh(meshVertices, meshTriangles, meshIndices);
+        DrawCollider(meshVertices, meshTriangles);
 
     }
 
     void CloneAsteroid(GameObject mainAsteroid)
     {
         Asteroid mainAst = mainAsteroid.GetComponent<Asteroid>();
-        DrawMesh(mainAst.meshVertices, mainAst.meshTriangles);
-
+        meshVertices = mainAst.meshVertices;
+        meshTriangles = mainAst.meshTriangles;
+        meshIndices = mainAst.meshIndices;
+        DrawMesh(meshVertices, meshTriangles, meshIndices);
+        DrawCollider(meshVertices, meshTriangles);
+        
     }
-    void DrawMesh(Vector3[] vertices, int[] triangles)
+    void DrawMesh(Vector3[] vertices, int[] triangles, int[] meshIndices)
     {
         mesh = new Mesh();
         this.gameObject.GetComponent<MeshFilter>().mesh = mesh;
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+
+        MeshFilter outlineFilter = asteroidOutlines.GetComponent<MeshFilter>();
+        Mesh mesh2 = new Mesh();
+        outlineFilter.mesh = mesh2;
+        mesh2.vertices = vertices;
+        mesh2.SetIndices(meshIndices, MeshTopology.Lines, 0);
+
+
+
     }
 
+    void DrawCollider(Vector3[] vertices, int[] triangles)
+    {
+        colliderMesh = mesh;
+        meshCollider = this.gameObject.GetComponent<MeshCollider>();
+        meshCollider.sharedMesh = mesh;
+    }
 
     // Update is called once per frame
     void Update()
@@ -149,6 +192,7 @@ public class Asteroid : MonoBehaviour
     void UpdateAsteroidRotation()
     {
         float frameRotation = Time.deltaTime * rotationRate;
+
         this.gameObject.transform.Rotate(new Vector3(0, 0, frameRotation));
     }
 }
