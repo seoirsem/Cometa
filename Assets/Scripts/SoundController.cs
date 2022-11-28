8a8b1d21b2004358bdea0b25ba90b327500393cb
seoirsem <54598583+seoirsem@users.noreply.github.com>
2022-11-28 14:45:36 +0000

    added song

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -6,14 +6,22 @@ public class SoundController : MonoBehaviour
 {
  
     AudioSource audioSource;
+    AudioSource musicSource;
+
     AudioClip asteroidCollision;
     AudioClip explosion;
+    AudioClip music;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        musicSource = transform.Find("MusicController").GetComponent<AudioSource>();
         asteroidCollision = Resources.Load<AudioClip>("Sounds/rockImpact");
         explosion = Resources.Load<AudioClip>("Sounds/explosion");
+        music = Resources.Load<AudioClip>("Sounds/song_pixabay");
+
+        //ToDo: menu and game music. Music ramps up as you play
+        StartMusic();
     }
 
     // Update is called once per frame
@@ -22,6 +30,12 @@ public class SoundController : MonoBehaviour
         
     }
 
+    void StartMusic()
+    {
+        musicSource.clip = music;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
 
     public void asteroidCollisionSound()
     {
